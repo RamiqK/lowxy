d9abf724bb0e14e0deebc8acb7cced50c014cda1
RamiqK <43069933+RamiqK@users.noreply.github.com>
2019-07-15 13:23:38 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import os
+bot = commands.Bot(command_prefix='!')
+bot.remove_command("help")
+ 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="!ininal - !ininaldestek"))
+    print(bot.user.name)
+    print(bot.user.id)
+    print('Bot is ready.')
+
+@bot.command()
+async def ininal(ctx):
+    await ctx.send('4091870393183')
+
+@bot.command()
+async def ininaldestek(ctx):
+    await ctx.send('İninali olmayan arkadaşlar, ininal alırsanız "mb161358" kodunu kullanınca hem TMU hem siz kazanabilirsiniz!
+
+bot.run(os.getenv('BOT_TOKEN'))
